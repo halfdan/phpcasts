9e08390bc62a3cfb8668b8965f457782113e9966
Fabian Becker <halfdan@xnorfz.de>
2011-03-25 21:05:09 +0100

    Added _video partial to render each video-tag seperately. Changed some layout Added OAuth with github Renamed main.haml to episodes.haml

--- main.rb
+++ main.rb
@@ -2,7 +2,9 @@ require 'rubygems'
 require 'sinatra'
 require 'sequel'
 require 'haml'
-require 'action_view'
+require 'omniauth'
+require 'rack-flash'
+require 'lib/partial'
 
 configure do
 	Sequel.connect(ENV['DATABASE_URL'] || 'sqlite://phpcasts.db')
@@ -11,41 +13,84 @@ configure do
 	PHPCast = OpenStruct.new(
 		:title => 'PHPcasts',
 		:author => 'Fabian Becker',
-		:url_base => 'http://localhost:4567/',
-		:admin_password => 'changeme',
-		:admin_cookie_key => 'phpcasts_admin',
-		:admin_cookie_value => '51d6d976913ace58'
+		:url_base => 'http://phpcasts.net',
+		:github_id => 'github_id',
+		:github_secret => 'github_secret'
 	)
 end
 
-helpers do
-  include Rack::Utils
-  alias_method :h, :escape_html
+enable :sessions
 
-  include ActionView::Partials
+use OmniAuth::Builder do
+	# Sign up at https://github.com/account/applications
+	provider :github, PHPCast.github_id, PHPCast.github_secret
+end
+
+helpers do
+	include Rack::Utils
+	alias_method :h, :escape_html
+
+	include Sinatra::Partials
+
+	use Rack::Flash
+
+	# url_escape helper
+	def u(url)
+		CGI.escape url
+	end
+
+	def is_logged_in?
+		session[:logged_in] == true
+	end
+
+	def username
+		session[:username]
+	end
+
+	def current_url
+		request.url
+	end
 end
 
 # Load Models
 require 'models/Episode.rb'
 require 'models/Video.rb'
+require 'models/User.rb'
+require 'models/Comment.rb'
 
 layout 'layout'
 
 get '/' do
-	episodes = Episode.order(:created_at.desc).all
-	haml :main, :locals => { :episodes => episodes }
+	@episodes = Episode.order(:created_at.desc).all
+	haml :episodes
+#main, :locals => {:episodes => episodes}
 end
 
 get '/episode/:id' do
-	episode = Episode.filter(:id => params[:id]).first
-	haml :episode, :locals => { :episode => episode }
+	@episode = Episode.filter(:id => params[:id]).first
+	haml :episode
 end
+
+get '/login' do
+	session[:return_to] = params[:return_to] if params[:return_to]
+	redirect '/auth/github'
+end
+
+get '/auth/:name/callback' do
+	auth = request.env['omniauth.auth']
+	user = User.filter(:github_uid => auth["uid"]).first || User.create_from_omniauth(auth)
+	session[:username] = user.name
+	session[:logged_in] = true
+	flash[:notice] = "You successfully logged in!"
+	redirect (!session[:return_to].nil? && session[:return_to]) || PHPCast.url_base
+end
+
 
 get '/about' do
 	haml :about
 end
 
-get '/feed' do
+get '/feed.rss' do
 	episodes = Episode.filter(:published => true).order(:created_at.desc).all
-	haml :feed, :locals => { :episodes => episodes }, :layout => false
+	haml :feed, :locals => {:episodes => episodes}, :layout => false
 end
